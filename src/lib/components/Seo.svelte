ec9263aa6fc7023e9f657a0877372b05682a7c4a
Jordan Baron <jordannbaron@gmail.com>
2021-12-01 22:25:29 -0500

    Add more SEO tags

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -4,5 +4,12 @@
 
 <svelte:head>
 	<title>{title}</title>
+	<meta
+		name="description"
+		content="I'm an 18 year-old web developer from Virginia that utilizes the latest technologies to build beautiful, high performant web apps"
+	/>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=7" />
+	<meta name="robots" content="index, follow" />
 	<script src="https://kit.fontawesome.com/68cef23f73.js" crossorigin="anonymous"></script>
 </svelte:head>
