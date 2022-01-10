180af706f128a64c3f8aca779defd6a481ae2b33
Jordan Baron <jordannbaron@gmail.com>
2022-01-10 11:05:42 -0500

    Make text larger on larger screens

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -10,7 +10,7 @@
 	<img src={img} alt="project thumbnail" />
 	<div class="p-4">
 		<h1 class="text-2xl lg:text-xl font-semibold">{name}</h1>
-		<p class="text-sm lg:text-xs my-3">{description}</p>
+		<p class="text-sm lg:text-xs 2xl:text-sm my-3">{description}</p>
 		<div class="pt-2 lg:text-sm">
 			<a href={codeUrl} class="font-semibold underline pr-2">Code</a>
 			<a href={previewUrl} class="font-semibold underline">Preview</a>
