cf87e81007b2f1cb04ad06820dbdeac55687575f
Jordan Baron <jordannbaron@gmail.com>
2022-01-12 18:33:12 -0500

    Make text larger on smaller screens

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -9,10 +9,10 @@
 	class="bg-[#0D0D0D] py-4 px-6 mt-4 mb-12 rounded-xl shadow-md flex flex-col h-42 md:h-[15.625rem] relative"
 >
 	<h1 class="font-bold text-xl md:text-2xl">{name}</h1>
-	<p class="my-2 text-xs md:text-base">{description}</p>
+	<p class="my-2 text-sm md:text-base">{description}</p>
 
 	<div
-		class="font-bold text-right text-sm md:text-base flex self-end md:absolute md:bottom-0 md:pb-6"
+		class="font-bold text-right text- md:text-base flex self-end pt-2 md:pt-0 md:absolute md:bottom-0 md:pb-6"
 	>
 		<a href={codeUrl} class="underline mx-2">code</a>
 		<a href={previewUrl} class="underline">preview</a>
