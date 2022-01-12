6ed8a5404c6c805b03ffb865ac66e4f905ad399e
Jordan Baron <jordannbaron@gmail.com>
2022-01-12 18:50:30 -0500

    Add hover color change on social media icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -22,13 +22,13 @@
 				<p class="">18 year-old web developer from Virginia</p>
 				<div class="text-xl pt-4">
 					<a href="https://twitter.com/codedbyjordan" class="px-2"
-						><i class="fab fa-twitter fa-2x" /></a
+						><i class="fab fa-twitter fa-2x transition duration-150 hover:text-gray-400" /></a
 					>
 					<a href="https://github.com/codedbyjordan" class="px-2"
-						><i class="fab fa-github fa-2x" /></a
+						><i class="fab fa-github fa-2x transition duration-150 hover:text-gray-400" /></a
 					>
 					<a href="https://linkedin.com/in/jordanmbaron" class="px-2"
-						><i class="fab fa-linkedin-in fa-2x" /></a
+						><i class="fab fa-linkedin-in fa-2x transition duration-150 hover:text-gray-400" /></a
 					>
 				</div>
 			</div>
