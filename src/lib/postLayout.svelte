944e4db0130d552a511c7f881c5d08ee29c7887a
Jordan Baron <jordannbaron@gmail.com>
2022-01-13 13:22:24 -0500

    Add layout for post

---
+++ src/lib/postLayout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount } from 'svelte';
+	import hljs from 'highlight.js';
+
+	export let title;
+	export let subtitle;
+	export let date;
+
+	onMount(() => {
+		hljs.highlightAll();
+	});
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/themes/github-dark.css" />
+</svelte:head>
+
+<div class="mt-12">
+	<p>{date}</p>
+	<div class="text-xl mt-4 prose prose-sm prose-invert">
+		<slot />
+	</div>
+</div>
