6f9389506267c677a0540ee2ac53b7f8e34c4fc9
Jordan Baron <jordannbaron@gmail.com>
2022-01-13 13:21:50 -0500

    Change purge to content for tailwind 3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 const config = {
 	mode: 'jit',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
@@ -20,7 +20,7 @@ const config = {
 		}
 	},
 
-	plugins: []
+	plugins: [require('@tailwindcss/typography')]
 };
 
 module.exports = config;
