b1cded343f4d3a5a78e15c2321c5aa3bbf39a3b8
Jordan Baron <jordannbaron@gmail.com>
2022-02-28 18:00:47 -0500

    Add Sanity Studio source

---
+++ studio/schemas/project.js
@@ -0,0 +1,27 @@
+export default {
+	title: 'Project',
+	name: 'project',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string'
+		},
+		{
+			title: 'Description',
+			name: 'description',
+			type: 'string'
+		},
+		{
+			title: 'Code URL',
+			name: 'code_url',
+			type: 'url'
+		},
+		{
+			title: 'Preview URL',
+			name: 'preview_url',
+			type: 'url'
+		}
+	]
+};
