7892c125564840cde57d22087af4403f30212ffb
Jordan Baron <jordannbaron@gmail.com>
2022-03-25 12:32:40 -0400

    remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	extensions: ['.svelte'],
 
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -14,14 +11,10 @@ const config = {
 		preprocess({
 			postcss: true
 		}),
-		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
 		adapter: vercel(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
 	}
 };
 
