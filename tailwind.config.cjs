baef664304225606f517c05c5de3c2ff5f96bc3c
Jordan Baron <jordannbaron@gmail.com>
2023-09-05 00:02:52 -0400

    full v3 in one commit cuz why not

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": {
+            transform: "translateY(50%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: 100,
+          },
+        },
+      },
+      animation: {
+        "slide-up": "slide-up 0.6s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
