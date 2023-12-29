881d8c7088ebba22f04eb751263e6dfe97b08dd8
Jordan Baron <jordannbaron@gmail.com>
2023-12-29 18:38:03 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
-      theme: "monokai",
-    },
+      theme: "monokai"
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
