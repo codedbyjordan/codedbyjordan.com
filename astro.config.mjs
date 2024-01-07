7812d4bd4877ddd91f9e379e8f31c4b30c0ec34c
Jordan Baron <jordannbaron@gmail.com>
2024-01-07 12:34:22 -0500

    basic ass image preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   markdown: {
     shikiConfig: {
-      theme: "monokai",
-    },
+      theme: "monokai"
+    }
   },
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
